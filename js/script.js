cedb6c4fd24422882ad5962079af0f3919d41e42
Rodrigo Soares <rodrigosoares.dev@gmail.com>
2023-04-28 16:27:37 -0300

    Efect slide in stacks

--- js/script.js
+++ js/script.js
@@ -8,3 +8,51 @@ function toggleMenu() {
 }
 
 btnMobile.addEventListener('click', toggleMenu);
+
+
+// Show skills header
+const skills = document.querySelector('.section-header .skills');
+const stacks = document.querySelectorAll('.stacks li');
+const images = document.querySelectorAll('.section-header .skills li img');
+
+
+Array.prototype.forEach.call(stacks, (element) => {
+    const attr = element.getAttribute('id');
+    element.addEventListener('mouseover', () => showSkills(attr));
+    element.addEventListener('mouseout', () => showSkills(attr, false));
+});
+
+function activeStackFromSkill(skill) {
+    const category = skill.dataset.skillCategory;
+        const categories = category.split(', ');
+
+        categories.forEach(cat => {
+            const stack = document.getElementById(cat);
+            stack.classList.toggle('active');
+        });
+}
+
+Array.prototype.forEach.call(images, (img) => {
+    img.addEventListener('mouseover', () => activeStackFromSkill(img));
+    img.addEventListener('mouseout', () => activeStackFromSkill(img));
+});
+
+
+function showSkills(stack, show = true) {
+    Array.prototype.forEach.call(images, (skill) => {
+        const category = skill.dataset.skillCategory;
+        const categories = category.split(', ');
+
+        skill.classList.remove('active');
+
+        if (show && categories.includes(stack)) {
+            skill.classList.add('active');
+            skills.classList.add('active');
+        }
+
+        if (!show) {
+            skills.classList.remove('active');
+        }
+
+    });
+}
